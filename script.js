87328c29bb5a1c6963a26e621180cdfdd42346c8
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-10 22:00:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-//your JS code here. If required.
-let inputnumber=prompt("input number");
-function gg(inputnumber){
-	if(inputnumber%3===0&&inputnumber%5===0)
-{
-	alert(inputnumber"FizzBuzz");
+let inputnumber = prompt("input number");
+
+function gg(inputnumber) {
+  if (inputnumber % 3 === 0 && inputnumber % 5 === 0) {
+    alert(inputnumber + "\\nFizzBuzz");
+  } else if (inputnumber % 3 === 0) {
+    alert(inputnumber + "\\nFizz");
+  } else if (inputnumber % 5 === 0) {
+    alert(inputnumber + "\\nBuzz");
+  }
 }
-if(inputnumber%3===0)
-{
-	alert(inputnumber"Fizz");
-}
-if(inputnumber%5===0)
-{
-	alert(inputnumber"Buzz");
-}};
+
+gg(inputnumber);
