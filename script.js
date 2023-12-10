7628db73da8c90116053b14d9314cf89be4a4be7
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-10 21:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let inputnumber=prompt("input number");
+if(inputnumber%3===0&&inputnumber%===0)
+{
+	alert(inputnumber"FizzBuzz");
+}
+if(inputnumber%3===0)
+{
+	alert(inputnumber"Fizz");
+}
+if(inputnumber%5===0)
+{
+	alert(inputnumber"Buzz");
+}
